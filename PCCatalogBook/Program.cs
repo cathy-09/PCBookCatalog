cde4b333bfce51cf5f28cb81c4b404beb2c8a8e5
5io <p.baklarov@gmail.com>
2024-02-26 21:35:01 +0200

    testt

--- PCCatalogBook/Program.cs
+++ PCCatalogBook/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World12312312!");
+            Console.WriteLine("Hello, World12312312123!");
         }
     }
 }
