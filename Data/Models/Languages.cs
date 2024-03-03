418f89fbf7d1d2c19432d15dd35060f17e54f7d9
CozDoog101 <matteoivanov06@gmail.com>
2024-03-03 22:01:24 +0200

    g

---
+++ Data/Models/Languages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Languages
+    {
+        [Key]
+        public int LanguagesId { get; set; }
+        [Required]
+        public int LanguagesCode { get; set;}
+        [Required]
+        public int LanguagesName { get; set; }
+
+    }
+}
