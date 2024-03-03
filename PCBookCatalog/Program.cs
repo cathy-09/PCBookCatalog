4670d749f7a37235b6ebf16afe9a2b512a66dcb3
1234 <1234@DESKTOP-JVVNO60>
2024-03-03 16:00:02 +0200

    Project directory

---
+++ PCBookCatalog/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PCBookCatalog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
