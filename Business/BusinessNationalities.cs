3b9db848d05f638a7e7024aa5c7bfdf91b5ad76f
1234 <1234@DESKTOP-JVVNO60>
2024-03-03 18:28:44 +0200

    Business Nationalities Update

--- Business/BusinessNationalities.cs
+++ Business/BusinessNationalities.cs
@@ -3,11 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Business
 {
     public class BusinessNationalities
     {
-
+        private BookCatalogContext bookCatalogContext;
+        public List<Nationalities> GetAll()
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                return bookCatalogContext.Nationalities.ToList();
+            }
+        }
+        public Nationalities Get(int id)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                return bookCatalogContext.Nationalities.Find(id);
+            }
+        }
+        public void Add(Nationalities nationalities)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                bookCatalogContext.Nationalities.Add(nationalities);
+                bookCatalogContext.SaveChanges();
+            }
+        }
+        public void Update(Nationalities nationalities)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                Nationalities item = bookCatalogContext.Nationalities.Find(nationalities.NationalityId);
+                if (item != null)
+                {
+                    bookCatalogContext.Entry(item).CurrentValues.SetValues(nationalities);
+                    bookCatalogContext.SaveChanges();
+                }
+            }
+        }
+        public void Delete(int id)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                Nationalities nationalities = bookCatalogContext.Nationalities.Find(id);
+                if (nationalities != null)
+                {
+                    bookCatalogContext.Nationalities.Remove(nationalities);
+                    bookCatalogContext.SaveChanges();
+                }
+            }
+        }
     }
 }
