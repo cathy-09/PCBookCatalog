dfc3f2356097ae991214f9044f3edab53c907921
5io <p.baklarov@gmail.com>
2024-03-03 22:27:54 +0200

    PublishersBusiness

--- Business/PublishersBusiness.cs
+++ Business/PublishersBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data.Models;
+using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,53 @@ namespace Business
 {
     public class PublishersBusiness
     {
+        private BookCatalogContext bookCatalogContext;
 
+        public List<Publishers> GetAllPublishers()
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                return bookCatalogContext.Publishers.ToList();
+            }
+        }
+        public Publishers GetPublishers(int id)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                return bookCatalogContext.Publishers.Find(id);
+            }
+        }
+        public void AddPublishers(Publishers publishers)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                bookCatalogContext.Publishers.Add(publishers);
+                bookCatalogContext.SaveChanges();
+            }
+        }
+        public void UpdatePublishers(Publishers publishers)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                Publishers item = bookCatalogContext.Publishers.Find(publishers.PublisherId);
+                if (item != null)
+                {
+                    bookCatalogContext.Entry(item).CurrentValues.SetValues(publishers);
+                    bookCatalogContext.SaveChanges();
+                }
+            }
+        }
+        public void DeletePublishers(int id)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                Publishers publishers = bookCatalogContext.Publishers.Find(id);
+                if (publishers != null)
+                {
+                    bookCatalogContext.Publishers.Remove(publishers);
+                    bookCatalogContext.SaveChanges();
+                }
+            }
+        }
     }
 }
