ec64dbd71faf6afc33a2d671127fefaa443554de
CozDoog101 <matteoivanov06@gmail.com>
2024-03-03 19:08:50 +0200

    Creating GenresBusiness and LanguagesBusiness + adding DbSet<Languages>

---
+++ Business/GenresBusiness.cs
@@ -0,0 +1,65 @@
+﻿using Data;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class GenresBusiness
+    {
+        private BookCatalogContext bookCatalogContext;
+
+        public List<Genres> GetAll()
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                return bookCatalogContext.Genres.ToList();
+            }
+        }
+        public Genres Get(int id)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                return bookCatalogContext.Genres.Find(id);
+            }
+        }
+        public void Add(Genres genres)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                bookCatalogContext.Genres.Add(genres);
+                bookCatalogContext.SaveChanges();
+            }
+        }
+        public void Update(Genres genres)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                Genres item = bookCatalogContext.Genres.Find(genres.Genres_id);
+                if (item != null)
+                {
+                    bookCatalogContext.Entry(item).CurrentValues.SetValues(genres);
+                    bookCatalogContext.SaveChanges();
+                }
+            }
+
+        }
+        public void Delete(int id)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                Genres genres = bookCatalogContext.Genres.Find(id);
+                if (genres != null)
+                {
+                    bookCatalogContext.Genres.Remove(genres);
+                    bookCatalogContext.SaveChanges();
+                }
+            }
+        }
+
+    }
+}
