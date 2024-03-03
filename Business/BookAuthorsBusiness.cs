5c16d08f0381a6b60526c2e4ff3c176bf94abfe3
5io <p.baklarov@gmail.com>
2024-03-03 22:19:14 +0200

    bookAuthorsBusiness

--- Business/BookAuthorsBusiness.cs
+++ Business/BookAuthorsBusiness.cs
@@ -27,5 +27,37 @@ namespace Business
                 return bookCatalogContext.BooksAuthors.Find(id);
             }
         }
+        public void AddBooksAuthors(BooksAuthors booksAuthors)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                bookCatalogContext.BooksAuthors.Add(booksAuthors);
+                bookCatalogContext.SaveChanges();
+            }
+        }
+        public void UpdateBooksAuthors(BooksAuthors booksAuthors)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                BooksAuthors item = bookCatalogContext.BooksAuthors.Find(booksAuthors.BookId, booksAuthors.AuthorId);
+                if (item != null)
+                {
+                    bookCatalogContext.Entry(item).CurrentValues.SetValues(booksAuthors);
+                    bookCatalogContext.SaveChanges();
+                }
+            }
+        }
+        public void DeleteBooksAuthors(int id)
+        {
+            using (bookCatalogContext = new BookCatalogContext())
+            {
+                BooksAuthors booksAuthors = bookCatalogContext.BooksAuthors.Find(id);
+                if (booksAuthors != null)
+                {
+                    bookCatalogContext.BooksAuthors.Remove(booksAuthors);
+                    bookCatalogContext.SaveChanges();
+                }
+            }
+        }
     }
 }
