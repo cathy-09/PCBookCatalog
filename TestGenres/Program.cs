a026b4fdad1d5dd86aedec279faafa94aab71fe2
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2024-03-16 11:45:51 +0200

    commit

---
+++ TestGenres/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGenres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
