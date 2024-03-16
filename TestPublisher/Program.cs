7c347486323c82b62436b91460c85982ba697f9a
Petio0 <p.baklarov@gmail.com>
2024-03-16 12:00:06 +0200

    TestPublishers

---
+++ TestPublisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestPublisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestPublisher testPublisher = new TestPublisher();
+        }
+    }
+}
