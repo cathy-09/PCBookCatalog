fc2d6c5165a4134e3a9e71a0a52249a15d0a1194
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2024-03-24 10:13:45 +0200

    UGUYG

--- Data/Models/Book.cs
+++ Data/Models/Book.cs
@@ -14,22 +14,14 @@ namespace Data.Models
 
         [Required]
         public string Name { get; set; }
-        private int genreId;
-        public int GenreId { set { genreId = value; } }
-        private int authorId;
-        public int AuthorId { set { authorId = value; } }
         public Author Author { get; set; }
         public Genre Genre { get; set; }
-        private int publisherId;
-        public int PublisherId { set { publisherId = value; } }
         public Publisher Publisher { get; set; }
         public double Rating { get; set; }
         public int Pages { get; set; }
         public decimal Price { get; set; }
         public string ISBN { get; set; }
         public int PublicationYear { get; set; }
-        private int languageId;
-        public int LanguageId { set { languageId = value; } }
         public Language Language { get; set; }
         public ICollection<BookAuthor> BookAuthors { set { BookAuthors = value; } }
     }
